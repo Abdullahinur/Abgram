732cf7ba1ea56e92415cf585683528950276e37b
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-04 11:33:27 +0300

    add view function to display todays date

--- pictures/views.py
+++ pictures/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Welcome to Abgram')
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
