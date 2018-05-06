75b7e302f053a2c2ed26c32260cc0d61df18ee0c
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-06 18:11:41 +0300

    fix models image and run test pass

--- pictures/models.py
+++ pictures/models.py
@@ -3,12 +3,43 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    pass
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_locations(self):
+        self.save()
+
+    def delete_locations(self):
+        self.delete()
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_categories(self):
+        self.save()
+
+    def delete_categories(self):
+        self.delete()
 
 
 class Image(models.Model):
-    pass
+    image = models.ImageField(upload_to='images/', null=True)
+    image_name = models.CharField(max_length=30, null=True)
+    image_description = models.CharField(max_length=3000, null=True)
+    location = models.ForeignKey(Location, null=True)
+    categories = models.ForeignKey(Category, null=True)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_images(self):
+        self.save()
+
+    def delete_images(self):
+        self.delete()
