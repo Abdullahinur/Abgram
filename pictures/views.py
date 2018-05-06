c5ed86d231fe458ef3b8640cfae914f3ebfc3dbe
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-06 21:41:28 +0300

    add template to display images on index

--- pictures/views.py
+++ pictures/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 import datetime as dt
 from django.shortcuts import render
-
+from .models import Category, Image, Location
 # Create your views here.
 
 
 def home(request):
     date = dt.date.today()
     slogan = 'Just keep uploading...'
-    return render(request, 'index.html', {"date": date, "slogan": slogan})
+    pictures = Image.get_images()
+    return render(request, 'index.html', {"date": date, "slogan": slogan, "pictures": pictures})
 
 
 def detail(request, image):
